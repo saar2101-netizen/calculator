18a1dac97c6f4baf1cf944c2cf2784e628f0fb45
Saar Efrat <saar2101@gmail.com>
2026-01-21 15:14:32 +0200

    multiply and divide in main

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from operations import add, subtract
+from operations import add, subtract, multiply, divide
 
 
 print("Welcome to calculator")
@@ -10,3 +10,7 @@ print(f"5 + 3 = {result1}")
 
 result2 = subtract(10, 4)
 print(f"10 - 4 = {result2}")
+
+print(f"6 * 7 = {multiply(6, 7)}")
+print(f"20 / 4 = {divide(20, 4)}")
+print(f"10 / 10 = {divide(10, 10)}")
