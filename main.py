a12e3e0a5c02336e5627457f54c9cfc2be0ff681
Saar Efrat <saar2101@gmail.com>
2026-01-21 15:09:55 +0200

    Add and subtract in main

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+from operations import add, subtract
 
 
 print("Welcome to calculator")
 print("==============================")
+
+
+result1 = add(5, 3)
+print(f"5 + 3 = {result1}")
+
+result2 = subtract(10, 4)
+print(f"10 - 4 = {result2}")
