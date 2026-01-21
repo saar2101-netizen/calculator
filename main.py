b23080254c6282aaa76f6d79de7e6f8701e1f172
Saar Efrat <saar2101@gmail.com>
2026-01-21 16:00:34 +0200

    Fix to main

--- main.py
+++ main.py
@@ -6,32 +6,35 @@ from ui import get_number,show_menu
 print("Welcome to calculator")
 print("==============================")
 
+def main():
+    while True:
+        choice = show_menu()
 
-while True:
-    choice = show_menu()
+        if choice == "0":
+            print("Good bye")
+            break
 
-    if choice == "0":
-        print("Good bye")
-        break
+        if choice in ["1","2","3","4","5"]:
+            num1 = get_number("Enter the first number: ")
+            num2 = get_number("Enter the second number: ")
 
-    if choice in ["1","2","3","4","5"]:
-        num1 = get_number("Enter the first number: ")
-        num2 = get_number("Enter the second number: ")
+            if choice == "1":
+                print(f"The result is {add(num1, num2)}")
+            elif choice == "2":
+                print(f"The result is {subtract(num1, num2)}")
+            elif choice == "3":
+                print(f"The result is {multiply(num1, num2)}")
+            elif choice == "4":
+                print(f"The result is {divide(num1, num2)}")
+            elif choice == "5":
+                print(f"The result is {power(num1, num2)}")
 
-        if choice == "1":
-            print(f"The result is {add(num1, num2)}")
-        elif choice == "2":
-            print(f"The result is {subtract(num1, num2)}")
-        elif choice == "3":
-            print(f"The result is {multiply(num1, num2)}")
-        elif choice == "4":
-            print(f"The result is {divide(num1, num2)}")
-        elif choice == "5":
-            print(f"The result is {power(num1, num2)}")
+        elif choice == "6":
+            num = get_number("Please enter the number: ")
+            print(f"The result is {square_root(num)}")
 
-    elif choice == "6":
-        num = get_number("Please enter the number: ")
-        print(f"The result is {square_root(num)}")
+        else:
+            print("ERROR: choice not valid!")
 
-    else:
-        print("ERROR: choice not valid!")
+if __name__ == "__main__":
+    main()
