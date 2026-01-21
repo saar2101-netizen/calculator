8aa65f19129edc610ba89abca46bf77acb2d6478
Saar Efrat <saar2101@gmail.com>
2026-01-21 15:16:40 +0200

    User interface

---
+++ ui.py
@@ -0,0 +1,23 @@
+
+# User interface
+
+def get_number(prompt):
+# getting user num
+    while True:
+        try:
+            return float(input(prompt))
+        except ValueError:
+            print("ERROR: Please enter a number!")
+
+
+def show_menu():
+# shoeing the menu
+    print("\n ===MENU===")
+    print("1. Add")
+    print("2. subtract")
+    print("3. Multiply")
+    print("4. Divide")
+    print("5. Power")
+    print("6. Square root")
+    print("0. Exit")
+    return input("Enter your choice")
