b3e0dbc5d14edfcc129a12a707e92af9c72a24fd
Saar Efrat <saar2101@gmail.com>
2026-01-21 15:16:02 +0200

    advance / advance added to main

---
+++ advanced.py
@@ -0,0 +1,16 @@
+
+def power(base, exponent):
+#   number in power
+    return base ** exponent
+
+
+def square_root(n):
+#   square root of a number
+    if n < 0:
+        return "ERROR: There is no square root to a minus number"
+    return n ** 0.5
+
+
+def modulo(a, b):
+#   modulo of two numbers
+    return a % b
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from operations import add, subtract, multiply, divide
+from advanced import power, square_root, modulo
 
 
 print("Welcome to calculator")
@@ -14,3 +15,9 @@ print(f"10 - 4 = {result2}")
 print(f"6 * 7 = {multiply(6, 7)}")
 print(f"20 / 4 = {divide(20, 4)}")
 print(f"10 / 10 = {divide(10, 10)}")
+
+print("\n Advanced operations")
+
+print(f"2 ^ 8 = {power(2, 8)}")
+print(f"√16 = {square_root(16)}")
+print(f"17 % 5 = {modulo(17, 5)}")
