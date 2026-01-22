8c5fc083a9e64f9e3d8f160feb56b16340c5006c
Saar Efrat <saar2101@gmail.com>
2026-01-22 10:05:22 +0200

    Adding the calculate the factorial of a number to advance

--- advanced.py
+++ advanced.py
@@ -18,3 +18,12 @@ def square_root(n):
 def modulo(a, b):
 #   modulo of two numbers
     return a % b
+
+def factorial(n):
+#   Calculate the factorial of a number
+    if n < 0:
+        return "ERROR: There is no factorial for negative numbers "
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
